6eaa163c8b69027a865ff27dffc8498430b27ace
samer <samerbenmim@insat.u-carthage.tn>
2022-09-16 22:57:57 +0100

    add props

---
+++ src/lib/User.svelte
@@ -0,0 +1,13 @@
+<script>
+   export let image;
+   export let age;
+   export let name;
+   export let index;
+</script>
+
+<div class="flex m-5 bg-white border p-2 rounded-lg shadow-md hover:shadow-lg cursor-pointer  items-center" >
+    <p class="w-10"> {1+index}</p>
+    <img src={user.image} alt="img" class="rounded-full w-14 h-14 mx-4">
+    <p class="mx-4 w-20">{user.name}</p>
+    <p  class="mx-4 w-20 ">{user.age}</p>
+</div>
--- src/lib/Users.svelte
+++ src/lib/Users.svelte
@@ -1,4 +1,7 @@
 <script>
+    import User from "./User.svelte";
+
+
     let users = [
     {
         image :"https://cdn-icons-png.flaticon.com/128/3135/3135715.png", 
@@ -26,13 +29,8 @@
     <h1 class="text-2xl text-center mt-10">
       List of users
     </h1>
-    {#each getUsers() as user , index}
-    <div class="flex m-5 bg-white border p-2 rounded-lg shadow-md hover:shadow-lg cursor-pointer  items-center" >
-        <p class="w-10"> {1+index}</p>
-        <img src={user.image} alt="img" class="rounded-full w-14 h-14 mx-4">
-        <p class="mx-4 w-20">{user.name}</p>
-        <p  class="mx-4 w-20 ">{user.age}</p>
-    </div>
+    {#each getUsers() as {image,name,age} , index}
+    <User {image} {index} {age} {name}/>
     {:else}
     <p>No user found</p>
     {/each}
