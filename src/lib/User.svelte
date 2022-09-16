d9ddf6dfdbfa2844fca37256a0325c10576692d1
samer <samerbenmim@insat.u-carthage.tn>
2022-09-17 00:03:27 +0100

    add select

--- src/lib/User.svelte
+++ src/lib/User.svelte
@@ -7,7 +7,7 @@
 
 <div class="flex m-5 bg-white border p-2 rounded-lg shadow-md hover:shadow-lg cursor-pointer  items-center" >
     <p class="w-10"> {1+index}</p>
-    <img src={user.image} alt="img" class="rounded-full w-14 h-14 mx-4">
-    <p class="mx-4 w-20">{user.name}</p>
-    <p  class="mx-4 w-20 ">{user.age}</p>
+    <img src={image} alt="img" class="rounded-full w-14 h-14 mx-4">
+    <p class="mx-4 w-20">{name}</p>
+    <p  class="mx-4 w-20 ">{age}</p>
 </div>
--- src/lib/Users.svelte
+++ src/lib/Users.svelte
@@ -6,30 +6,55 @@
     {
         image :"https://cdn-icons-png.flaticon.com/128/3135/3135715.png", 
         name: "John",
-        age: "27 129 130"
+        age: "27 129 130",
+        active : true
     },
     {
         image :"https://cdn-icons-png.flaticon.com/128/3135/3135715.png", 
         name: "John",
-        age: "27 129 130"
+        age: "27 129 130",
+        active : true,
+
+
     },
     {
         image :"https://cdn-icons-png.flaticon.com/128/3135/3135715.png", 
         name: "John",
-        age: "27 129 130"
+        age: "27 129 130",
+        active : false
+
     },
     ] ;
+    let filteredUsers = users;
 
-    const getUsers = ()=> {
-        return users;
-    }
 </script>
 
 <div>
     <h1 class="text-2xl text-center mt-10">
       List of users
     </h1>
-    {#each getUsers() as {image,name,age} , index}
+    <select
+    on:change="{(e) => {
+        const status= e.target.value=="true";
+
+        if(e.target.value ===null){
+            filteredUsers = users;
+            return
+        }
+        else{
+
+            filteredUsers = users.filter((user) => {
+                
+                return    user.active == status 
+            });
+        }
+    }}"
+    name="user-filter" class="border-rounded-lg px-4 py-2 ml-4" >
+        <option value={null}>ALL</option>
+        <option value={true}>ACTIVE</option>
+        <option value={false}>INACTIVE</option>
+    </select>
+    {#each filteredUsers as {image,name,age} , index}
     <User {image} {index} {age} {name}/>
     {:else}
     <p>No user found</p>
