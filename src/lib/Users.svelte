815dfc6d4a609c992224a9b54abb1425115fbe8f
samer <samerbenmim@insat.u-carthage.tn>
2022-09-16 22:47:12 +0100

    add tailwind

--- src/lib/Users.svelte
+++ src/lib/Users.svelte
@@ -3,17 +3,17 @@
     {
         image :"https://cdn-icons-png.flaticon.com/128/3135/3135715.png", 
         name: "John",
-        age: 30
+        age: "27 129 130"
     },
     {
         image :"https://cdn-icons-png.flaticon.com/128/3135/3135715.png", 
         name: "John",
-        age: 30
+        age: "27 129 130"
     },
     {
         image :"https://cdn-icons-png.flaticon.com/128/3135/3135715.png", 
         name: "John",
-        age: 30
+        age: "27 129 130"
     },
     ] ;
 
@@ -23,15 +23,15 @@
 </script>
 
 <div>
-    <h1>
+    <h1 class="text-2xl text-center mt-10">
       List of users
     </h1>
     {#each getUsers() as user , index}
-    <div >
-        <p> {1+index}</p>
-        <img src={user.image} alt="img">
-        <p>{user.name}</p>
-        <p>{user.age}</p>
+    <div class="flex m-5 bg-white border p-2 rounded-lg shadow-md hover:shadow-lg cursor-pointer  items-center" >
+        <p class="w-10"> {1+index}</p>
+        <img src={user.image} alt="img" class="rounded-full w-14 h-14 mx-4">
+        <p class="mx-4 w-20">{user.name}</p>
+        <p  class="mx-4 w-20 ">{user.age}</p>
     </div>
     {:else}
     <p>No user found</p>
